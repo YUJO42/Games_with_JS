a9c24e25a5d151d083fc2d42974c9054e1dd0392
yujo <yujo@student.42seoul.kr>
2020-09-25 21:16:48 +0900

    Add Logig matched three block

--- Candy Crush/app.js
+++ Candy Crush/app.js
@@ -2,6 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
   const grid = document.querySelector('.grid');
   const width = 8;
   const squares = [];
+  let score = 0;
 
   const candyColors = ['red', 'yellow', 'orange', 'purple', 'green', 'blue'];
 
@@ -84,7 +85,53 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   // Checking for matches
-  // check for row of Three
 
-  //
+  // Check for row of Three
+  function checkRowForTree() {
+    for (let i = 0; i < 61; i++) {
+      let rowOfThree = [i, i + 1, i + 2];
+      let decidedColor = squares[i].style.backgroundColor;
+      const isBlank = squares[i].style.backgroundColor === '';
+
+      if (
+        rowOfThree.every(
+          (index) =>
+            squares[index].style.backgroundColor === decidedColor && !isBlank,
+        )
+      ) {
+        score += 3;
+        rowOfThree.forEach((index) => {
+          squares[index].style.backgroundColor = '';
+        });
+      }
+    }
+  }
+  checkRowForTree();
+
+  // Check for column of Three
+  function checkColumnForTree() {
+    for (let i = 0; i < 47; i++) {
+      let columnOfThree = [i, i + width, i + width * 2];
+      let decidedColor = squares[i].style.backgroundColor;
+      const isBlank = squares[i].style.backgroundColor === '';
+
+      if (
+        columnOfThree.every(
+          (index) =>
+            squares[index].style.backgroundColor === decidedColor && !isBlank,
+        )
+      ) {
+        score += 3;
+        columnOfThree.forEach((index) => {
+          squares[index].style.backgroundColor = '';
+        });
+      }
+    }
+  }
+  checkColumnForTree();
+
+  window.setInterval(function () {
+    checkRowForTree();
+    checkColumnForTree();
+  }, 100);
 });
